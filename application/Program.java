e6a4f6d0ab782cc4ab441d682964d81cf55e6ffa
biancasuarz <blancasuarz@gmail.com>
2024-10-06 18:49:48 -0300

    First Commit

---
+++ application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.println("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for(int i= 1; i <= n; i++){
+            System.out.println("Employee #"+i+ "data:");
+            System.out.print("Outsourced (y/n)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+        }
+
+        sc.close();
+    }
+}
