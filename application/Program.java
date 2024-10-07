04618ff18ea06cfae10cbed756a90ca3bf394461
biancasuarz <blancasuarz@gmail.com>
2024-10-07 09:09:13 -0300

    Last Commit

--- application/Program.java
+++ application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import entities.Employee;
+import entities.OutsourcedEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,16 +20,31 @@ public class Program {
         System.out.println("Enter the number of employees: ");
         int n = sc.nextInt();
 
-        for(int i= 1; i <= n; i++){
-            System.out.println("Employee #"+i+ "data:");
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Employee #" + i + "data:");
             System.out.print("Outsourced (y/n)? ");
             char ch = sc.next().charAt(0);
             System.out.print("Name: ");
             sc.nextLine();
             String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            System.out.println("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            if (ch == 'y') {
+                System.out.println("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                list.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+            } else {
+                list.add(new Employee(name, hours, valuePerHour));
+            }
 
+            System.out.println();
+            System.out.println("PAYMENTS: ");
+            for (Employee emp : list) {
+                System.out.println(emp.getName() + "- $" + String.format("%.2f", emp.payment()));
+            }
         }
-
         sc.close();
     }
 }
